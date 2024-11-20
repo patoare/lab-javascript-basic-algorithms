ab28150e852d0f6f2a0449f81a6b599987d28202
patricio <patoare@hotmail.com>
2024-11-20 18:58:05 +0100

    lab2 finished

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 // Iteration 1: Names and Input
+/*driver it's Patricio and navigator is Maria */
 
+const hacker1 = "Patricio";
+
+console.log(`The driver's name is ${hacker1}`);
+
+const hacker2 = "Maria";
+
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
+if (hacker1 > hacker2) {
+console.log(`The driver has the longest name, it has ${hacker1.length} characters`);
+} else {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length}characters.`);
+}
+
 
 // Iteration 3: Loops
+const letter = hacker1.split("");
+const makeStr = letter.toString();
+const mayusText = makeStr.toUpperCase();
+console.log(mayusText);
+
+
+const letter2 = hacker2.split("");
+
+const reverse = letter.reverse();
+console.log(reverse);
